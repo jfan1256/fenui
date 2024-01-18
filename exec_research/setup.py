b1f0fce8e828d2f8c976eb7813723679d2e35b5d
jofan23 <jonathan.fan@yale.edu>
2024-01-17 22:57:27 -0500

    refactored the directory

---
+++ exec_research/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Researching novel methods to generate uncertainty indexes',
+    author='Jonathan Fan, Yinan Su, Leland Bybee',
+    license='',
+)
--- exec_research/utils/system.py
+++ exec_research/utils/system.py
@@ -2,11 +2,11 @@ from pathlib import Path
 
 # Get root directory
 def get_root_dir() -> Path:
-    return Path(__file__).resolve().parent.parent
+    return Path(__file__).resolve().parent.parent.parent
 
 # Get data directory
 def get_data():
-    return Path(__file__).resolve().parent.parent.parent / 'data'
+    return get_root_dir() / 'data'
 
 # Get data directory
 def get_format_data():
