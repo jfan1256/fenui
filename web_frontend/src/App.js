b5c00786ad3b772afe114de7c447243ae306d16a
jofan23 <jonathan.fan@yale.edu>
2024-07-15 01:50:46 -0400

    updated ngrok

--- web_frontend/src/App.js
+++ web_frontend/src/App.js
@@ -97,7 +97,7 @@ function App() {
             // const response = await axios.post('https://webbackend-yhzjtissga-ue.a.run.app/generate_plot', { "input_str": userInput });
 
             // Prod (Ngrok)
-            const response = await axios.post("https://408d-128-255-234-12.ngrok-free.app/generate_plot", { "input_str": userInput });
+            const response = await axios.post("https://f861-128-255-234-12.ngrok-free.app/generate_plot", { "input_str": userInput });
 
             // Parse the plot JSON data
             const plotData = JSON.parse(response.data.gen_plot);
